b5facd68dfeb05196e229de0b51578f710fd45fb
erwinvaneyk <erwinvaneyk@gmail.com>
2015-05-27 11:27:21 +0200

    Added additional constructor to ClusterFactoryImpl to allow custom IPs

--- src/main/java/nl/erwinvaneyk/core/ClusterFactoryImpl.java
+++ src/main/java/nl/erwinvaneyk/core/ClusterFactoryImpl.java
@@ -1,5 +1,8 @@
 package nl.erwinvaneyk.core;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import nl.erwinvaneyk.communication.BasicMessage;
 import nl.erwinvaneyk.communication.Message;
 import nl.erwinvaneyk.communication.exceptions.CommunicationException;
@@ -10,14 +13,28 @@ import nl.erwinvaneyk.communication.rmi.RMISocket;
 public class ClusterFactoryImpl implements ClusterFactory {
 
 	private final NodeFactory nodeFactory;
+	private final String localIp;
+
+	public ClusterFactoryImpl(NodeFactory nodeFactory, String localIp) {
+		this.nodeFactory = nodeFactory;
+		this.localIp = localIp;
+	}
 
 	public ClusterFactoryImpl(NodeFactory nodeFactory) {
+		String localIp;
 		this.nodeFactory = nodeFactory;
+		try {
+			localIp = InetAddress.getLocalHost().getHostAddress();
+		}
+		catch (UnknownHostException e) {
+			localIp = "127.0.0.1";
+		}
+		this.localIp = localIp;
 	}
 
 	@Override
 	public Node startCluster(int port, String clusterId) throws CommunicationException {
-		NodeAddress nodeAddress = new NodeAddressImpl(NodeImpl.NODE_TYPE, 0, Address.getMyAddress(port));
+		NodeAddress nodeAddress = new NodeAddressImpl(NodeImpl.NODE_TYPE, 0, new Address(localIp, port));
 		return nodeFactory.get(nodeAddress, clusterId);
 	}
 
@@ -29,7 +46,7 @@ public class ClusterFactoryImpl implements ClusterFactory {
 		RMISocket socket = new RMISocket(address);
 		Message response = socket.sendRequest(reserveIdMessage);
 		// Create node based on response
-		NodeAddress nodeAddress = new NodeAddressImpl(nodeFactory.getNodeType(), (Integer) response.getOrThrow("id"), Address.getMyAddress(port));
+		NodeAddress nodeAddress = new NodeAddressImpl(nodeFactory.getNodeType(), (Integer) response.getOrThrow("id"), new Address(localIp, port));
 		Node node = nodeFactory.get(nodeAddress, (String) response.getOrThrow("clusterId"));
 		// connect to other nodes
 		node.getState().getConnectedNodes().addAll(
